b7408e47e7a05812eea4262419b3fcf4c2602dfc
swlws <979623074@qq.com>
2025-05-08 22:01:36 +0800

    PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  apps: [{
-    name: 'chat',
-    script: 'server.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: 'websocket-chat',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }]
+  ],
 };
